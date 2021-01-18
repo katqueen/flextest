6992ecab8a3df96bb6d58d448bc03e55a4ebe426
vijay <Vijayaraj.Govindharaj@flexagon.com>
2021-01-18 10:56:31 -0600

    FLXTEST-5 changed to force commit for tracking issues test - VJ 01/18

--- EBSCustomizations/XXHR/java/xxt/oracle/apps/ak/xxperson/schema/server/xxPersonDetailsEOImpl.java
+++ EBSCustomizations/XXHR/java/xxt/oracle/apps/ak/xxperson/schema/server/xxPersonDetailsEOImpl.java
@@ -16,6 +16,7 @@ public class xxPersonDetailsEOImpl extends OAEntityImpl
   protected static final int FIRSTNAME = 1;  
   protected static final int LASTNAME = 2;
   protected static final int EMAIL = 3;
+  
   protected static final int STUDENTFLAG = 4;
   protected static final int NAMEOFUNIVERSITY = 5;
   protected static final int LASTUPDATEDATE = 6;
