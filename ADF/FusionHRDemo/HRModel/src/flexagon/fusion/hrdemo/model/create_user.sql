4e4701cfde27647b22b98e66f604aacee2e46a3e
vijay <Vijayaraj.Govindharaj@flexagon.com>
2021-02-02 10:15:19 -0600

    for testing

--- ADF/FusionHRDemo/HRModel/src/flexagon/fusion/hrdemo/model/create_user.sql
+++ ADF/FusionHRDemo/HRModel/src/flexagon/fusion/hrdemo/model/create_user.sql
@@ -8,7 +8,9 @@ grant CREATE SESSION to HRDEMO;
 grant all privileges on HR.DEPARTMENTS to HRDEMO;
 grant all privileges on HR.COUNTRIES to HRDEMO;
 grant all privileges on HR.EMPLOYEES to HRDEMO;
+
 grant all privileges on HR.JOB_HISTORY to HRDEMO;
 grant all privileges on HR.JOBS to HRDEMO;
 grant all privileges on HR.LOCATIONS to HRDEMO;
+
 grant all privileges on HR.REGIONS to HRDEMO;
